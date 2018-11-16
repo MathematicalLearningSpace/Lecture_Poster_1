4985746f2a6e61ac22ba73e63d13bae5d68c49e3
Jeff Cromwell, PhD <designofharmony@gmail.com>
2018-11-16 02:58:59 -0700

    Update Theory_Conditional_Probability_Moments_3.R

--- Theory_Conditional_Probability_Moments_3.R
+++ Theory_Conditional_Probability_Moments_3.R
@@ -1,19 +1,7 @@
-library(xtable)
-library(vars)
-library(tseries)
-library(urca)
-library(fUnitRoots)
-library(tsDyn)
-library(tseriesChaos)
-library(fNonlinear)
-library(stats)
-library(Fractal)
-library(fracdiff)
-library(fSeries)
-library(stringr)
-library(stringi)
-library(corrplot)
-library(ggplot2)
+#---------------------------------------R API -----------------------------------------------------------
+library(xtable);library(vars);library(tseries);library(urca);library(fUnitRoots);library(tsDyn)
+library(tseriesChaos);library(fNonlinear);library(stats);library(Fractal);library(fracdiff)
+library(fSeries);library(stringr);library(stringi);library(corrplot);library(ggplot2)
 #---------------------------------------------------Data----------------------------------------------------
 
 Gene.Expression<-read.delim("dataPlusScores_all5.txt")
@@ -30,14 +18,11 @@ UBE2D2.Gene.Description<-Gene.Description[indx.2[2]]
 
 UBC<-t(Gene.Expression[indx[2],53:100])
 UBE2D2<-t(Gene.Expression[indx.2[2],53:100])
-
 #--------------------------------------------------Modeling and Analysis-----------------------------------
 Gene.Study<-as.matrix(cbind(UBC,UBE2D2))
 colnames(Gene.Study)<-c('UBC','UBE2D2')
 Gene.Study.Trans<-as.matrix(cbind(diff(UBC),diff(UBE2D2)))
-
 M <- cor(Gene.Study)
-
 Gene.Network.Causality<-gene.causality(Gene.Study,1)
 Gene.Network.Impulses<-gene.impulse.response(Gene.Study)
 
@@ -49,13 +34,9 @@ Gene.Network.Causality.df<-rbind(c(Gene.Network.Causality$Granger.hypothesis,
                                    Gene.Network.Causality$Instant.statistic,
                                    Gene.Network.Causality$Instant.pvalue))
 colnames(Gene.Network.Causality.df)<-c("Hypothesis","F-Test","P-values")
-#---------------------------------------------------Tables-------------------------------------------------
-
-
+#---------------------------------------------------Tables------------------------------------------------
 Table.1<-xtable(Gene.Network.Causality.df)
-
-
-#--------------------------------------------------Figures-------------------------------------------------
+#---------------------------------Figures for Classroom-------------------------------------------------
 Figure.1<-plot(UBC,type="l",lty=1,col=1,ylab="x values")
 lines(UBE2D2,lty=2,col=2)
 grid()
@@ -66,8 +47,7 @@ Figure.2<-corrplot(M, order="hclust", addrect=3,col=1:5,t1.cex=0.8)
 title(main="Gene Correlation Plot")
 Figure.3<-boxplot(Gene.Study)
 Figure.4<-plot(Gene.Network.Impulses$irfs)
-
-#-------------------------------------------------Function Library------------------------------------------
+#---------------------------Function Library for Student Modification------------------------------------------
 gene.causality<-function(x,lagOrder)
 {
     var.1<-VAR(x, lag.max = lagOrder, type="const")
