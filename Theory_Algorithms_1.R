3c3a4c9eebd9b4600903972d98fdb8979e136e81
Jeff Cromwell, PhD <designofharmony@gmail.com>
2018-11-16 07:03:57 -0700

    Update Theory_Algorithms_1.R

--- Theory_Algorithms_1.R
+++ Theory_Algorithms_1.R
@@ -1,7 +1,6 @@
 #-------------------------------------R API ------------------------------------------------------------------
 library(pracma); library(xtable)
 #--------------------------------------------Data-------------------------------------------------------------
-
 parm.1<-c(1,-1)
 parm.2<-c(1,-1,0.5)
 f.1<- function(t, y)
@@ -15,20 +14,14 @@ y0.1 <- as.matrix(c(0,1))
 y0.2 <- as.matrix(c(0, 1, 1))
 t0 <- 0; tf <- 20
 #-------------------------------------------Computation------------------------------------------------------
-
 solution.1<- ode23(f.1, t0, tf, y0.1, rtol=1e-5, atol=1e-10)
 solution.2<- ode23.mutation(f.2, t0, 0.95, tf,y0.2, rtol=1e-5, atol=1e-10)
-
 model.solution.df<-data.frame()
 model.solution.df<-cbind(solution.2$step,solution.2$h,solution.2$threshold)
 colnames(model.solution.df)<-c("Step Size","h","Threshold")
-
 #--------------------------------------------Tables-----------------------------------------------------------
-
 Table.1<-xtable(model.solution.df)
-
 #--------------------------------------------Figures for Classroom Presentation----------------------------------------------------------
-
 Figure.1<-matplot(solution.1$t, solution.1$y, type = "l", lty = 1, lwd = c(2, 1),
         col = c("darkred", "darkblue"),
         xlab = "Time [min]", ylab= "",
@@ -87,7 +80,7 @@ ode23.mutation<-function (f, t0, stepSize,tfinal, y0, ..., rtol = 0.001, atol = 
     #--------------------Evaluate-------------------------------------------------------
     e <- h * (-5 * s1 + 6 * s2 + 8 * s3 - 9 * s4)/72
     err <- max(abs(e/max(max(abs(y), abs(ynew)), threshold))) + realmin
-    #-----------------------------Check the Error---------------------------------------
+    #---------------Check the Error-----------------------------------------------------
     if (err <= rtol) {
       t <- tnew
       y <- ynew
@@ -102,7 +95,7 @@ ode23.mutation<-function (f, t0, stepSize,tfinal, y0, ..., rtol = 0.001, atol = 
       t <- tfinal
     }
   }
-  #--------------------Basic Return math objects pattern------------------------
+  #-------  Basic Return math objects pattern for modification in class--------
   return(list(t = c(tout), 
               y = yout,
               step=stepSize,
