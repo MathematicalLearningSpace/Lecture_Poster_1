a8a17fa433abce45dfb0765e21b9a2c6359e03d9
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-02-24 15:22:08 -0700

    Update Probability_Theory_2.R

--- Probability_Theory_2.R
+++ Probability_Theory_2.R
@@ -1,3 +1,4 @@
+#-----------------------------R Code To Modify in the Classroom Lecture with Students-----------------------
 #------------------------------------R API ----------------------------------------------------------------
 library(PearsonDS); library(copula);library(VineCopula);library(vines);library(xtable)
 #------------------------------------Data Generation-------------------------------------------------------
