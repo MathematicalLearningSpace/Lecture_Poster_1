ad3be244f06bc8b425d322177da567335614472c
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-02-24 15:17:23 -0700

    Update OrdinaryDifferentialEquation_Theory_11.R

--- OrdinaryDifferentialEquation_Theory_11.R
+++ OrdinaryDifferentialEquation_Theory_11.R
@@ -1,3 +1,4 @@
+#-----------------------------R Code To Modify in the Classroom Lecture with Students-----------------------
 #----------------------------------R API ----------------------------------------------
 library(readr);library(xtable);library(deSolve);library(ReacTran);library(rootSolve);library(fda);library(phaseR)
 library(xtable);library(tseriesChaos);library(corrplot);library(plot3D);library(scatterplot3d)
