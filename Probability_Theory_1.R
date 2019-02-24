20cbd9cee48e4795aa498b0398af61f9d52cdceb
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-02-24 15:21:38 -0700

    Update Probability_Theory_1.R

--- Probability_Theory_1.R
+++ Probability_Theory_1.R
@@ -1,3 +1,4 @@
+#-----------------------------R Code To Modify in the Classroom Lecture with Students-----------------------
 #-----------------------------------R API -----------------------------------------------------------
 library(PearsonDS); library(fitdistrplus); library(xtable)
 #-------------------------------------Probability Theory: Pearson Distributions----------------------------------------------
