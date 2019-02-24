f984c94c4da500a4954fc0b3aa749786be45376f
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-02-24 15:13:55 -0700

    Update Theory_Conditional_Probability_Moments_1.R

--- Theory_Conditional_Probability_Moments_1.R
+++ Theory_Conditional_Probability_Moments_1.R
@@ -1,4 +1,5 @@
-#-----------------------------R API ---------------------------------------------
+#-----------------------------R Code To Modify in the Classroom Lecture with Students-----------------------
+#-----------------------------R API ------------------------------------------------------------------------
 library(Matrix);library(igraph);library(rbenchmark);library(xtable);library(stringi)
 library(PearsonDS);library(h2o);library(darch);library(deepnet);library(caret)
 #---------------------------------------------Data----------------------------------------------------------------------
