354a73f8e71f8924f58fcac0b83d5fe640c85305
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-02-25 11:48:56 -0700

    Update OrdinaryDifferentialEquation_Theory_1.R

--- OrdinaryDifferentialEquation_Theory_1.R
+++ OrdinaryDifferentialEquation_Theory_1.R
@@ -108,6 +108,25 @@ Figure.4<-plot3d(BCC.system.solution.1[,2],
        pch = 1, cex = 1, xlab = "x(t)", 
        ylab = "y(t)", zlab = "z(t)")
 
-
 rgl.snapshot("BCC_Model_1.png")
 #--------------------------------------Function Library-----------------------------------------------
+#-------------Function Template Library for Classroom Presentation and Modification---------------------
+f.1<-function(X)
+ {
+  Z<-""
+  a<-1
+  W<-runif(length(X),0,1)
+  for(i in 1:length(X))
+  {  
+	Z<-stringr::str_c(Z,X[i])
+	W[i]<-a*W[i]
+  }
+  output<-list()
+  output$X<-X
+  output$a<-a
+  output$Z<-Z
+  output$W<-W
+  return(output)
+ } 
+test.f.1<-f.1(letters)
+test.f.1
