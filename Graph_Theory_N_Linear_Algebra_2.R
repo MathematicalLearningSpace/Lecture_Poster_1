8238cabede3fedd27a0561a8c7aa711149656027
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-02-25 04:07:06 -0700

    Update Graph_Theory_N_Linear_Algebra_2.R

--- Graph_Theory_N_Linear_Algebra_2.R
+++ Graph_Theory_N_Linear_Algebra_2.R
@@ -1,13 +1,5 @@
-library(xtable)
-library(igraph)
-library(Matrix)
-library(qgraph)
-library(qlcMatrix)
-library(rcrossref)
-library(PearsonDS)
-library(catnet)
-library(CCA)
 
+library(xtable);library(igraph);library(Matrix);library(qgraph);library(qlcMatrix);library(rcrossref);library(PearsonDS);library(catnet);library(CCA)
 #-------------------------------------------------Data Matrix------------------------------------------------------
 A.matrix<-matrix(c(0.5,0.3,0.4,0.7,0.7,1,
             0.7,0.5,1,0.8,0.9,0.3,
@@ -35,6 +27,7 @@ A.singular<-eigen(A.matrix.skew)
 A.singular.max<-A.singular$values[1]
 
 w<-matrix(1,6,1)
+#------------------------Matrix Operations----------------------------------
 A.Decomp<-w%*%t(w)
 A.Var<-(t(w)%*%t(A.SK)%*%A.SK%*%w)/t(w)%*%w
 A.alpha<-A.Sy+(1-2*alpha)*A.SK
@@ -77,7 +70,7 @@ G.Perersen.K<-skewpart(t(graph.adjM.1))
 
 graph.sample.2<-make_ring(10)
 graph.adjM.2<-as_adjacency_matrix(graph.sample.2,sparse=FALSE)
-
+#----------------------------------------Second Moment Analysis-------------------------------------------
 matrix.correlation<-cor(c(as.matrix(graph.adjM.1)), c(as.matrix(graph.adjM.2)))
 matrix.correlation.test<-cor.test(c(as.matrix(graph.adjM.1)), c(as.matrix(graph.adjM.2)))
 cancor(as.matrix(graph.adjM.1), as.matrix(graph.adjM.2))
@@ -110,19 +103,25 @@ Figure.2<-plot(graph.sample,
      edge.arrow.size=0.5,
      main='Petersen Graph')
 
-
-
 #--------------------------------------------Function Library---------------------------------------------
-
+f.1<-function(X)
+{
 A.alpha.Perron.root.max<-NULL
 alpha.range<-seq(0,0.5,by=0.01)
 A<-NULL
 for(i in 1:length(alpha.range))
 {
-  A.alpha.Perron.root<-eigen((1-alpha.range[i])*A.matrix+alpha.range[i]*t(A.matrix))
+  A.alpha.Perron.root<-eigen((1-alpha.range[i])*X+alpha.range[i]*t(X))
   A.alpha.Perron.root.max[i]<-A.alpha.Perron.root$values[1]
   A[i]<-(1-alpha.range[i])+alpha.range[i]
 }
-
-
+output<-list()
+output$X<-X
+output$A.alpha.Perron.root<-A.alpha.Perron.root
+output$A.alpha.Perron.root.max<-A.alpha.Perron.root.max
+output$A<-A
+return(output)
+}
+test.f.1<-f.1(A.matrix)
+test.f.1
 
