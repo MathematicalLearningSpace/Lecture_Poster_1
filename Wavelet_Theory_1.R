994862c2e0987ee559e9e55e669b33d4587eb081
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-02-25 04:35:53 -0700

    Update Wavelet_Theory_1.R

--- Wavelet_Theory_1.R
+++ Wavelet_Theory_1.R
@@ -1,3 +1,4 @@
+#------------------R Code To Modify in the Classroom Lecture with Students--------------
 #---------------------------------------R API ---------------------------------------------
 library(wmtsa);library(wavelets);library(wavethresh);library(waveslim);library(wavemulcor)
 library(PearsonDS);library(xtable)
@@ -12,6 +13,41 @@ experimental.data.1<-window(error.pearson.3, end=256)
 x.dwt <- wavDWT(as.ts(experimental.data.1), n.levels = 8)
 x.modwt <- wavMODWT( as.ts(experimental.data.1), n.levels = 8)
 x.cwt.1 <- wavCWT( as.ts(experimental.data.1), wavelet="gaussian2")
+
+#--------------------------------------Models and Optimization---------------------------
+#-----------------------Maximum Likelihood Estimation---------------------------
+pearson.MLE.1 <- pearsonFitML(x.modwt$data$d1)
+pearson.MLE.2 <- pearsonFitML(x.modwt$data$d2)
+pearson.MLE.3 <- pearsonFitML(x.modwt$data$d3)
+pearson.MLE.4 <- pearsonFitML(x.modwt$data$d4)
+pearson.MLE.5 <- pearsonFitML(x.modwt$data$d5)
+pearson.MLE.6 <- pearsonFitML(x.modwt$data$d6)
+pearson.MLE.7 <- pearsonFitML(x.modwt$data$s3)
+
+pearson.MLE.dist<-c(pearson.MLE.1$type,
+                    pearson.MLE.2$type,
+                    pearson.MLE.3$type,
+                    pearson.MLE.4$type,
+                    pearson.MLE.5$type,
+                    pearson.MLE.6$type)
+
+#-----------------------------Method of Moments Estimation------------------------------
+#----------------Distribution MOM Identification---------------------------------------
+
+pearson.MOM.1<-pearsonFitM(moments=empMoments(x.modwt$data$d1))
+pearson.MOM.2<-pearsonFitM(moments=empMoments(x.modwt$data$d2))
+pearson.MOM.3<-pearsonFitM(moments=empMoments(x.modwt$data$d3))
+pearson.MOM.4 <- pearsonFitM(moments=empMoments(x.modwt$data$d4))
+pearson.MOM.5 <- pearsonFitM(moments=empMoments(x.modwt$data$d5))
+pearson.MOM.6 <- pearsonFitM(moments=empMoments(x.modwt$data$d6))
+pearson.MOM.7<-pearsonFitM(moments=empMoments(x.modwt$data$s3))
+
+pearson.MOM.dist<-c(pearson.MOM.1$type,
+                    pearson.MOM.2$type,
+                    pearson.MOM.3$type,
+                    pearson.MOM.4$type,
+                    pearson.MOM.5$type,
+                    pearson.MOM.6$type)
 #---------------------------------------Tables------------------------------------------
 wavelet.statistics.df<-data.frame()
 wavelet.statistics.moments.df<-data.frame()
@@ -45,42 +81,8 @@ wavelet.statistics.df<-cbind(wavelet.statistics.moments.df,
                              wavelet.statistics.entropy.df)
 Table.1<-xtable(wavelet.statistics.df)
 
-
-pearson.MLE.1 <- pearsonFitML(x.modwt$data$d1)
-pearson.MLE.2 <- pearsonFitML(x.modwt$data$d2)
-pearson.MLE.3 <- pearsonFitML(x.modwt$data$d3)
-pearson.MLE.4 <- pearsonFitML(x.modwt$data$d4)
-pearson.MLE.5 <- pearsonFitML(x.modwt$data$d5)
-pearson.MLE.6 <- pearsonFitML(x.modwt$data$d6)
-pearson.MLE.7 <- pearsonFitML(x.modwt$data$s3)
-
-pearson.MLE.dist<-c(pearson.MLE.1$type,
-                    pearson.MLE.2$type,
-                    pearson.MLE.3$type,
-                    pearson.MLE.4$type,
-                    pearson.MLE.5$type,
-                    pearson.MLE.6$type)
-
-#--------------------------------------------------Distribution MOM Identification
-
-pearson.MOM.1<-pearsonFitM(moments=empMoments(x.modwt$data$d1))
-pearson.MOM.2<-pearsonFitM(moments=empMoments(x.modwt$data$d2))
-pearson.MOM.3<-pearsonFitM(moments=empMoments(x.modwt$data$d3))
-pearson.MOM.4 <- pearsonFitM(moments=empMoments(x.modwt$data$d4))
-pearson.MOM.5 <- pearsonFitM(moments=empMoments(x.modwt$data$d5))
-pearson.MOM.6 <- pearsonFitM(moments=empMoments(x.modwt$data$d6))
-pearson.MOM.7<-pearsonFitM(moments=empMoments(x.modwt$data$s3))
-
-pearson.MOM.dist<-c(pearson.MOM.1$type,
-                    pearson.MOM.2$type,
-                    pearson.MOM.3$type,
-                    pearson.MOM.4$type,
-                    pearson.MOM.5$type,
-                    pearson.MOM.6$type)
-
 distribution.table.df<-data.frame()
-distribution.table.df<-cbind(pearson.MLE.dist,
-                             pearson.MOM.dist)
+distribution.table.df<-cbind(pearson.MLE.dist, pearson.MOM.dist)
 Table.2<-xtable(distribution.table.df)
 
 #-----------------MultiResolution Decomposition for Classroom Presentation--------------------
@@ -88,7 +90,7 @@ Figure.1<-plot(wavMRD(x.modwt, level = c(1:6)),col=c(1:7))
 abline(v = 100, lty = 2)
 abline(v = 250, lty = 3, col="Orange")
 abline(v = 400, lty = 2)
-
+#-----------------Figure Group 1-------------------------------------
 par(mfrow = c(2, 2))
 Figure.2<-plotdist(x.modwt$data$d1, histo = TRUE, demp = TRUE)
 plotdist(x.modwt$data$d2, histo = TRUE, demp = TRUE)
