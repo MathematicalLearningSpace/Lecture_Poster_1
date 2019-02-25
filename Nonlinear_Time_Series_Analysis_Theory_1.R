108b2b4e667927a69baf32ffc069cfbd142399c6
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-02-25 05:14:27 -0700

    Update Nonlinear_Time_Series_Analysis_Theory_1.R

--- Nonlinear_Time_Series_Analysis_Theory_1.R
+++ Nonlinear_Time_Series_Analysis_Theory_1.R
@@ -1,22 +1,8 @@
-library(tseries)
-library(costat)
-library(locits)
-library(wbsts)
-library(forecast)
-library(tsoutliers)
-library(jmotif)
-library(TSclust)
-library(TSMining)
-library(ggplot2)
-library(tsDyn)
-library(tseriesChaos)
-library(yuima)
-library(DescTools)
-
-library(xtable)
-library(PearsonDS)
-library(fitdistrplus)
-library(psych)
+#-----------------------------R Code To Modify in the Classroom Lecture with Students-----------------------
+#---------------------------------------------------R API --------------------------------------------------------
+library(tseries);library(costat);library(locits);library(wbsts);library(forecast);library(tsoutliers);library(jmotif)
+library(TSclust);library(TSMining);library(ggplot2);library(tsDyn);library(tseriesChaos);library(yuima);library(DescTools)
+library(xtable);library(PearsonDS);library(fitdistrplus);library(psych)
 
 #------------------------------------------Nonlinear Time Series Analysis--------------------------------------
 
