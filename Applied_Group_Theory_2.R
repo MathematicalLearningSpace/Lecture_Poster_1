133020b6b2eb49d63924aeab6ba6efe1ca974130
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-05-22 06:02:27 -0700

    Add files via upload

---
+++ Applied_Group_Theory_2.R
@@ -0,0 +1,5 @@
+#-------------------------These are examples for Numerical Linear Algebra and Applied Group Theory Developed in the Lectures-----------
+#
+library(backShift); library(BiDag);library(CIEE);library(dagbag);library(dagR);library(GADAG);library(GA);library(HEMDAG)
+library(unifDAG);library(Matrix);library(xtable);library(PearsonDS);library(igraph);library(qgraph)
+#---------------------------------------Data------------------------------------------------------------
