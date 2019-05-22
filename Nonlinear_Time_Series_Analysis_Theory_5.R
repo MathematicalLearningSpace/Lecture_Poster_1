69d7d652f3009b6b42bb9a786074e07c88b43bba
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-05-22 05:40:26 -0700

    Add files via upload

---
+++ Nonlinear_Time_Series_Analysis_Theory_5.R
@@ -0,0 +1,9 @@
+#-----------------------------R Code To Modify in the Classroom Lecture with Students-----------------------
+#---------------------------------------------------R API --------------------------------------------------------
+library(tseries);library(costat);library(locits);library(wbsts);library(forecast);library(tsoutliers);library(jmotif)
+library(TSclust);library(TSMining);library(ggplot2);library(tsDyn);library(tseriesChaos);library(yuima);library(DescTools)
+library(xtable);library(PearsonDS);library(fitdistrplus);library(psych)
+
+#------------------------------------------Nonlinear Time Series Analysis--------------------------------------
+
+#------------------------------------------Data-----------------------------------------------------------------
