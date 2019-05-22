d337f861b627855ea8feb603a4d6ac2353d12547
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-05-22 05:21:17 -0700

    Add files via upload

---
+++ OrdinaryDifferentialEquation_Theory_8.R
@@ -0,0 +1,4 @@
+#-----------------------------R Code To Modify in the Classroom Lecture with Students-----------------------
+#----------------------------------R API --------------------------------------------------
+library(xtable); library(deSolve); library(igraph); library(Matrix); library(visNetwork);library(corrplot);library(plot3D);library(scatterplot3d)
+library(rgl)
