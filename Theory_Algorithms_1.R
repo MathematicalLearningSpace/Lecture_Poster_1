e37320446a2a0059c3e5e06393db037eab117fee
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-05-22 05:48:03 -0700

    Update Theory_Algorithms_1.R

--- Theory_Algorithms_1.R
+++ Theory_Algorithms_1.R
@@ -1,4 +1,6 @@
 #-----------------------------R Code To Modify in the Classroom Lecture with Students-----------------------
+#-----------------------------These are examples of algorithms for the classrooms to teach students---------
+#-----------------------------how to modify the algorithm for their notes-----------------------------------
 #-------------------------------------R API ------------------------------------------------------------------
 library(pracma); library(xtable)
 #--------------------------------------------Data-------------------------------------------------------------
